d03bed1d2d553b142662dc15329c18d90052e4a0
bl4ck-w0lf-project <hounmenouangelo@gmail.com>
2026-06-24 11:49:37 +0100

    Test d'inscription et connexion réussi

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Routes publiques
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+//  AUTH
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-// Routes protégées par Sanctum
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [AuthController::class, 'profile']);
+    // Routes protégées
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'profile']);
+    });
 });
